38f3a6c4e9c89dcf55f42330f29543c78ff0928e
kfc <kfc@example.com>
2025-06-05 15:49:46 +0900

    add FlashMemoryDevice as a member of Device Driver

--- src/main/java/DeviceDriver.java
+++ src/main/java/DeviceDriver.java
@@ -3,17 +3,21 @@
  */
 public class DeviceDriver {
 
+    FlashMemoryDevice flashMemoryDevice;
+
     public DeviceDriver(FlashMemoryDevice hardware) {
         // TODO: implement this method
+        this.flashMemoryDevice = hardware;
     }
 
     public byte read(long address) {
         // TODO: implement this method
         // Add...
-        return -1;
+        return flashMemoryDevice.read(address);
     }
 
     public void write(long address, byte data) {
         // TODO: implement this method
+        flashMemoryDevice.write(address, data);
     }
 }
